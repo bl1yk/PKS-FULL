563343af8fe1d6e949d799ff66b7350e1a79c8f1
Bl1yk <grisatovmasan@gmail.com>
2024-12-19 16:12:28 +0300

    Commit 1

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -6,37 +6,40 @@ class Product {
   final String productAbout;
   final String productListen;
   int quantity;
+  bool favourite;
   bool inCart;
 
   Product({required this.productId, required this.productTitle,
     required this.productImage,
     required this.productPrice, required this.productAbout,
-    required this.productListen, this.quantity = 0, this.inCart = false,
+    required this.productListen, this.quantity = 0, this.favourite = false, this.inCart = false,
   });
 
   Map<String, dynamic> toJson() {
     return {
-      'ID': productId,
-      'Title': productTitle,
-      'ImageURL': productImage,
-      'Price': productPrice,
-      'Description': productAbout,
-      'Specifications': productListen,
-      'Quantity': quantity,
-      'InCart': inCart,
+      'id': productId,
+      'title': productTitle,
+      'image_url': productImage,
+      'price': productPrice,
+      'description': productAbout,
+      'listen': productListen,
+      'quantity': quantity,
+      'is_favourite': favourite,
+      'in_cart': inCart,
     };
   }
 
   factory Product.fromJson(Map<String, dynamic> json){
     return Product(
-      productId: json['ID'],
-      productTitle: json['Title'],
-      productImage: json['ImageURL'],
-      productPrice: json['Price'],
-      productAbout: json['Description'],
-      productListen: json['Listen'],
-      quantity: json['Quantity'] ?? 0,
-      inCart: json['InCart'] ?? false,
+      productId: json['id'],
+      productTitle: json['title'],
+      productImage: json['image_url'],
+      productPrice: json['price'],
+      productAbout: json['description'],
+      productListen: json['listen'],
+      quantity: json['quantity'] ?? 0,
+      favourite: json['is_favourite'] ?? false,
+      inCart: json['in_cart'] ?? false,
     );
   }
 }
